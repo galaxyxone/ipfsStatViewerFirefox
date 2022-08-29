d9ded3b5fb996ff3ca2a8f13d37a23ff45b1aae1
Logan Lentz <logan.lentz1@gmail.com>
2022-08-28 22:22:35 -0500

    CORS Update no borrito

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-const cors = require('cors')
-App.arguments(cors())
+// const cors = require('cors')
+// App.arguments(cors())
+
+// ^^^ This didnt work so I took it off:( ^^^
